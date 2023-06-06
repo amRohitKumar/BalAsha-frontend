791293005530d65cf359aec70af430557204359e
Shubhranshu Animesh <77923668+shubhranshu-animesh@users.noreply.github.com>
2023-06-06 10:55:51 +0530

    Add files via upload

---
+++ src/components/header/header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./header.css";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="logo"></div>
+    </header>
+  );
+};
+
+export default Header;
